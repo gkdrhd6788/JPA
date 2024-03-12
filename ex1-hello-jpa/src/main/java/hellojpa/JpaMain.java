753590c9360c2c289e94eaa0894f82498562e047
Sooji Kwon <gkdrhd6788@gmail.com>
2024-03-13 02:12:26 +0900

    didit

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,45 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.changeTeam(team);
+            em.persist(member);
+
+//            team.addMember(member);   // member.changeTeam(team)랑 둘중 하나 선택한다.
+
+//            team.getMembers().add(member); setTeam에서 설정해줌
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
--- jpashop/src/main/java/jpa-book/Main.java
+++ jpashop/src/main/java/jpa-book/Main.java
@@ -1,3 +1,5 @@
+package jpa-book;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
